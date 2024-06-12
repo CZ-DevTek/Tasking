2ecee5a8a67f983cf6c2a0c8d37f2f28f9effbdb
Carlos García Pérez <carlosziok@gmail.com>
2024-06-12 11:43:53 +0300

    Initial Commit

---
+++ Tasking/TaskingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskingApp.swift
+//  Tasking
+//
+//  Created by Carlos Garcia Perez on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

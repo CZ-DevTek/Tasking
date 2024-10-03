a96675f1415756d52544d89039e98934d84bccee
Carlos García Pérez <carlosziok@gmail.com>
2024-10-04 01:14:07 +0300

    Adding Navigation

--- Tasking/View/TasksInProcessView.swift
+++ Tasking/View/TasksInProcessView.swift
@@ -12,34 +12,36 @@ struct TasksInProcessView: View {
     @Binding var selectedTab: Int
     
     var body: some View {
-        List(taskManager.sortedTasks) { task in
-            NavigationLink(destination: taskManager.destinationView(for: task)) {
-                HStack(alignment: .center) {
-                    Text(task.name)
-                        .foregroundColor(.white)
-                        .padding(12)
-                        .padding(.vertical, 4)
+        NavigationView {
+            List(taskManager.sortedTasks) { task in
+                NavigationLink(destination: taskManager.destinationView(for: task)) {
+                    HStack(alignment: .center) {
+                        Text(task.name)
+                            .foregroundColor(.white)
+                            .padding(12)
+                            .padding(.vertical, 4)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(taskManager.color(for: task))
+                    .cornerRadius(8)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(taskManager.color(for: task))
-                .cornerRadius(8)
+                .listRowInsets(EdgeInsets())
+                .onAppear {
+                    selectedTab = 2
+                }
             }
-            .listRowInsets(EdgeInsets())
-            .onAppear {
-                selectedTab = 2
+            .scrollContentBackground(.hidden)
+            .navigationTitle("Tasks In Process")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Tasks In Process")
+                        .font(.custom("Noteworthy-Bold", size: 34))
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
             }
+            .padding()
         }
-        .scrollContentBackground(.hidden)
-        .navigationTitle("Tasks In Process")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                Text("Tasks In Process")
-                    .font(.custom("Noteworthy-Bold", size: 34))
-                    .foregroundColor(.black)
-                    .frame(maxWidth: .infinity, alignment: .center)
-            }
-        }
-        .padding()
     }
 }
